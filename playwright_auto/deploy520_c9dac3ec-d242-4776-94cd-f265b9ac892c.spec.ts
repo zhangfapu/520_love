00cea8de42c0b94d5079de548b06770a96ed43cc
zhangfapu <zhangfapu@example.com>
2025-05-20 11:43:37 +0800

    fix: update index.html for GitHub Pages

---
+++ playwright_auto/deploy520_c9dac3ec-d242-4776-94cd-f265b9ac892c.spec.ts
@@ -0,0 +1,9 @@
+
+import { test } from '@playwright/test';
+import { expect } from '@playwright/test';
+
+test('deploy520_2025-05-20', async ({ page, context }) => {
+  
+    // Navigate to URL
+    await page.goto('https://pages.coding.net/');
+});
